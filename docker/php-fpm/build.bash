d2fc6b8f47fb238d18dd0bfdf6996a0a6369b013
Miles Wu <slion0725@gmail.com>
2025-07-27 16:42:46 +0800

    add fpm

---
+++ docker/php-fpm/build.bash
@@ -0,0 +1,21 @@
+docker rm -f app-fpm-local
+
+docker rmi app-fpm-local
+
+docker build \
+--build-arg ENV_FILE=local.env \
+-f ./docker/php-fpm/Dockerfile \
+-t app-fpm-local:latest .
+
+# amd64
+# --platform linux/amd64 \
+
+docker run --name app-fpm-local \
+-p 8000:8000 \
+-e APP_ENV=production \
+--restart unless-stopped \
+--network public-network \
+-d app-fpm-local:latest
+
+# check crontab
+# docker exec -it container crontab -l
