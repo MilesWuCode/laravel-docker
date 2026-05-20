26ad5413a9f64c420925e8a4d5b2b6cd76873e57
miles <slion0725@gmail.com>
2026-05-20 14:38:18 +0800

    upd

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Jobs\TestQueueJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-use App\Jobs\TestQueueJob;
 
 Route::get('/test-queue', function () {
     dispatch(new TestQueueJob)
